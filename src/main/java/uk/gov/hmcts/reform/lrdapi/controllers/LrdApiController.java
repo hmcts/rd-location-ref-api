c6e8073e33cf37bc216a74a5beb6828bfda29c65
AdilOoze <adil.oozeerally@hmcts.net>
2021-06-04 12:10:18 +0100

    RDCC-2804: adding API in Controller

--- src/main/java/uk/gov/hmcts/reform/lrdapi/controllers/LrdApiController.java
+++ src/main/java/uk/gov/hmcts/reform/lrdapi/controllers/LrdApiController.java
@@ -6,29 +6,40 @@ import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.lrdapi.controllers.advice.InvalidRequestException;
+import uk.gov.hmcts.reform.lrdapi.controllers.response.LrdBuildingLocationResponse;
 import uk.gov.hmcts.reform.lrdapi.controllers.response.LrdOrgInfoServiceResponse;
+import uk.gov.hmcts.reform.lrdapi.service.LrdBuildingLocationService;
 import uk.gov.hmcts.reform.lrdapi.service.LrdService;
 import uk.gov.hmcts.reform.lrdapi.util.ConstraintValidation;
+import uk.gov.hmcts.reform.lrdapi.util.ValidationUtils;
 
 import java.util.List;
 
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequestMapping(
-    path = "/refdata/location/orgServices"
+    path = "/refdata/location"
 )
 @RestController
 @Slf4j
 public class LrdApiController {
 
+    private static final String NumericRegex = "\\d+";
+
     @Autowired
     LrdService lrdService;
 
+    @Autowired
+    LrdBuildingLocationService buildingLocationService;
 
     @ApiOperation(
         value = "This API will retrieve service code details association with ccd case type",
@@ -61,17 +72,72 @@ public class LrdApiController {
             message = "Internal Server Error"
         )
     })
-    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    @GetMapping(
+        path = "/orgServices",
+        produces = APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<Object> retrieveOrgServiceDetails(
         @RequestParam(value = "serviceCode", required = false) String serviceCode,
         @RequestParam(value = "ccdCaseType", required = false) String ccdCaseType,
         @RequestParam(value = "ccdServiceNames", required = false) String ccdServiceNames) {
         log.info("Inside retrieveOrgServiceDetails");
-
         ConstraintValidation.validateInputParameters(serviceCode, ccdCaseType, ccdServiceNames);
         List<LrdOrgInfoServiceResponse> lrdOrgInfoServiceResponse = null;
         lrdOrgInfoServiceResponse = lrdService.retrieveOrgServiceDetails(serviceCode, ccdCaseType, ccdServiceNames);
         return ResponseEntity.status(200).body(lrdOrgInfoServiceResponse);
+    }
+
+    @ApiOperation(
+        value = "This API will retrieve a Building Location's details for the given epims ID",
+        authorizations = {
+            @Authorization(value = "ServiceAuthorization"),
+            @Authorization(value = "Authorization")
+        }
+    )
+    @ApiResponses({
+        @ApiResponse(
+            code = 200,
+            message = "Successfully retrieved a Building Location's details",
+            response = LrdOrgInfoServiceResponse.class,
+            responseContainer = "list"
+        ),
+        @ApiResponse(
+            code = 400,
+            message = "Bad Request"
+        ),
+        @ApiResponse(
+            code = 401,
+            message = "Forbidden Error: Access denied"
+        ),
+        @ApiResponse(
+            code = 404,
+            message = "No Building Location found with the given ID"
+        ),
+        @ApiResponse(
+            code = 500,
+            message = "Internal Server Error"
+        )
+    })
+    @GetMapping(
+        path = "/building-locations/epims/{epims_id}",
+        produces = APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Object> retrieveBuildingLocationDetailsByEpimsId(
+        @PathVariable(value = "epims_id") String epimsId) {
+
+        log.info("Obtaining building locations for epimm id: " + epimsId);
+
+        if (isEmpty(epimsId)) {
+            throw new InvalidRequestException("No epimm id provided");
+        }
+
+        if (!ValidationUtils.isStringInExpectedFormat(epimsId.strip(), NumericRegex)) {
+            throw new InvalidRequestException("epimm id is expected to be a number");
+        }
+
+        LrdBuildingLocationResponse response =
+            buildingLocationService.retrieveBuildingLocationByEpimsId(epimsId);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
