b88da262aa54414eef466d76b8875f1cb8897a88
AdilOoze <adil.oozeerally@hmcts.net>
2021-06-07 16:47:41 +0100

    RDCC-2785: created insert statements for Court Location Category

---
+++ src/main/resources/db/migration/V1_10__static_table_inserts.sql
@@ -0,0 +1,399 @@
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '1',
+    'Admin Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '2',
+    'Admiralty and Commercial Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '3',
+    'Agricultural Land and Drainage Tribunal',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '4',
+    'Asylum Support Appeals',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '5',
+    'Bankruptcy Court (High Court)',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '6',
+    'Business and Property Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '7',
+    'Central London County Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '8',
+    'Central London County Court (Bankruptcy)',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '9',
+    'Chancery Division',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '10',
+    'County Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '11',
+    'Court of Appeal Civil Division',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '12',
+    'Court of Appeal Criminal Division',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '13',
+    'Court of Protection',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '14',
+    'Criminal Injuries Compensation',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '15',
+    'Crown Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '16',
+    'Employment Appeal Tribunal (England and Wales)',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '17',
+    'Employment Tribunal',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '18',
+    'Family Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '19',
+    'Gangmasters Licensing Appeals',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '20',
+    'General Regulatory Chamber',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '21',
+    'High Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '22',
+    'Housing Centre',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '23',
+    'Immigration and Asylum Tribunal',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '24',
+    'Lands Tribunal',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '25',
+    'Magistrates Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '26',
+    'Patents Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '27',
+    'Probate',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '28',
+    'Queen''s Bench Division (General)',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '29',
+    'Queen''s Bench Division Administrative Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '30',
+    'Residential Property Tribunal',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '31',
+    'Social Security and Child Support Tribunal',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '32',
+    'Special Immigration Appeals Commission',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '33',
+    'Tax Tribunal',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '34',
+    'Technology & Construction Court',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '35',
+    'Tribunals',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '36',
+    'Upper Tier Immigration and Asylum Tribunal',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '37',
+    'Upper Tribunal (Administrative Appeals Chamber)',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '38',
+    'Upper Tribunal (Lands Chamber)',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '39',
+    'Upper Tribunal (Tax and Chancery Chamber)',
+    ' ')
+;
+
+insert into court_location_category(
+    court_location_category_id,
+    court_location_category,
+    welsh_court_location_category)
+values(
+    '40',
+    'War Pensions & Armed Forces Compensation Chamber',
+    ' ')
+;
