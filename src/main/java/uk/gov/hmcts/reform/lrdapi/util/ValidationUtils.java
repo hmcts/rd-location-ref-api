489e21352524c267feecb065b577c4f03b689d88
viswajit-kuna <viswajit.kuna@HMCTS.NET>
2021-06-08 16:39:43 +0100

    RDCC-2928: Updated ValidationUtils and its test cases

--- src/main/java/uk/gov/hmcts/reform/lrdapi/util/ValidationUtils.java
+++ src/main/java/uk/gov/hmcts/reform/lrdapi/util/ValidationUtils.java
@@ -1,17 +1,17 @@
 package uk.gov.hmcts.reform.lrdapi.util;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class ValidationUtils {
 
     private ValidationUtils() {
     }
 
-    public static boolean isStringInExpectedFormat(String stringToEvaluate, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(stringToEvaluate);
-        return matcher.matches();
+    public static List<String> findInvalidIdentifiers(List<String> epimsIdList, String regex) {
+        return epimsIdList.stream()
+            .filter(id -> !id.matches(regex))
+            .collect(Collectors.toList());
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/lrdapi/util/ValidationUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/lrdapi/util/ValidationUtilsTest.java
@@ -2,46 +2,93 @@ package uk.gov.hmcts.reform.lrdapi.util;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ValidationUtilsTest {
 
-    private String regexNumeric = "\\d+";
+    private static final String AlphaNumericRegex = "[0-9a-zA-Z_]+";
 
     @Test
-    public void testInvalidEpimsIdInput_Alphanumeric() {
-        assertThat(ValidationUtils.isStringInExpectedFormat("1QWERTY", regexNumeric))
-            .isEqualTo(Boolean.FALSE);
+    public void testFindInvalidIdentifiers_SingleIdGiven_InvalidInputSpecialChars() {
+        List<String> identifiers = new ArrayList<>();
+        identifiers.add("!@£$");
+        assertThat(ValidationUtils.findInvalidIdentifiers(identifiers, AlphaNumericRegex))
+            .isEqualTo(identifiers);
     }
 
     @Test
-    public void testInvalidEpimsIdInput_Alphabetic() {
-        assertThat(ValidationUtils.isStringInExpectedFormat("QWERTY", regexNumeric))
-            .isEqualTo(Boolean.FALSE);
+    public void testFindInvalidIdentifiers_SingleIdGiven_ValidInputChars() {
+        List<String> identifiers = new ArrayList<>();
+        identifiers.add("QWERTY");
+        assertThat(ValidationUtils.findInvalidIdentifiers(identifiers, AlphaNumericRegex).size())
+            .isEqualTo(0);
     }
 
     @Test
-    public void testInvalidEpimsIdInput_Space() {
-        assertThat(ValidationUtils.isStringInExpectedFormat(" ", regexNumeric))
-            .isEqualTo(Boolean.FALSE);
+    public void testFindInvalidIdentifiers_SingleIdGiven_ValidInputNumeric() {
+        List<String> identifiers = new ArrayList<>();
+        identifiers.add("1234");
+        assertThat(ValidationUtils.findInvalidIdentifiers(identifiers, AlphaNumericRegex).size())
+            .isEqualTo(0);
     }
 
     @Test
-    public void testInvalidEpimsIdInput_SpecialCharacters() {
-        assertThat(ValidationUtils.isStringInExpectedFormat("!@$$£", regexNumeric))
-            .isEqualTo(Boolean.FALSE);
+    public void testFindInvalidIdentifiers_SingleIdGiven_ValidInputAlphaNumeric() {
+        List<String> identifiers = new ArrayList<>();
+        identifiers.add("qwerty1234");
+        assertThat(ValidationUtils.findInvalidIdentifiers(identifiers, AlphaNumericRegex).size())
+            .isEqualTo(0);
     }
 
     @Test
-    public void testInvalidEpimsIdInput_NegativeIntegers() {
-        assertThat(ValidationUtils.isStringInExpectedFormat("-1", regexNumeric))
-            .isEqualTo(Boolean.FALSE);
+    public void testFindInvalidIdentifiers_SingleIdGiven_ValidInputAlphaNumericUnderscore() {
+        List<String> identifiers = new ArrayList<>();
+        identifiers.add("qwerty_1234");
+        assertThat(ValidationUtils.findInvalidIdentifiers(identifiers, AlphaNumericRegex).size())
+            .isEqualTo(0);
     }
 
     @Test
-    public void testValidEpimsIdInput() {
-        assertThat(ValidationUtils.isStringInExpectedFormat("1", regexNumeric))
-            .isEqualTo(Boolean.TRUE);
+    public void testFindInvalidIdentifiers_MultipleIdsGiven_OneInvalidIdSpecialChars() {
+        List<String> identifiers = new ArrayList<>();
+        identifiers.add("1234");
+        identifiers.addAll(getSingleInvalidIdList());
+        assertThat(ValidationUtils.findInvalidIdentifiers(identifiers, AlphaNumericRegex))
+            .isEqualTo(getSingleInvalidIdList());
     }
+
+    @Test
+    public void testFindInvalidIdentifiers_MultipleIdsGiven_SomeInValidInputSpecialChars() {
+        List<String> identifiers = new ArrayList<>();
+        identifiers.add("QWERTY");
+        identifiers.addAll(getMultipleInvalidIdList());
+        assertThat(ValidationUtils.findInvalidIdentifiers(identifiers, AlphaNumericRegex))
+            .isEqualTo(getMultipleInvalidIdList());
+    }
+
+    @Test
+    public void testFindInvalidIdentifiers_MultipleIdsGiven_AllInvalidIdsSpecialChars() {
+        List<String> identifiers = new ArrayList<>();
+        identifiers.addAll(getMultipleInvalidIdList());
+        assertThat(ValidationUtils.findInvalidIdentifiers(identifiers, AlphaNumericRegex))
+            .isEqualTo(getMultipleInvalidIdList());
+    }
+
+    private List<String> getSingleInvalidIdList() {
+        List<String> invalidIdList = new ArrayList<>();
+        invalidIdList.add("!@£$");
+        return invalidIdList;
+    }
+
+    private List<String> getMultipleInvalidIdList() {
+        List<String> invalidIdList = new ArrayList<>();
+        invalidIdList.addAll(getSingleInvalidIdList());
+        invalidIdList.add("@£$%");
+        return invalidIdList;
+    }
+
 
 }
