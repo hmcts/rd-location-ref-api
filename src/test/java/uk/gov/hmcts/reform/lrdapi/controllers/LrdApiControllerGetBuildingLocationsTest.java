8710affa1810c5894fd3d6a352ed76c14c69a25e
viswajit-kuna <viswajit.kuna@HMCTS.NET>
2021-06-09 10:55:10 +0100

    RDCC-2928: Added tests for the new service method getAllBuildingLocations() - Also renamed the service method retrieveBuildingLocationByEpimsId() to retrieveBuildingLocationByEpimsIds()

--- src/test/java/uk/gov/hmcts/reform/lrdapi/controllers/LrdApiControllerGetBuildingLocationsTest.java
+++ src/test/java/uk/gov/hmcts/reform/lrdapi/controllers/LrdApiControllerGetBuildingLocationsTest.java
@@ -16,7 +16,6 @@ import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -36,18 +35,18 @@ public class LrdApiControllerGetBuildingLocationsTest {
     }
 
     @Test
-    public void testGetBuildingLocations_ValidEpimmsIdGiven_ShouldReturnStatusCode200() {
-        when(lrdBuildingLocationService.retrieveBuildingLocationByEpimsId(anyList()))
+    public void testGetBuildingLocations_ValidSingleEpimmsIdGiven_ShouldReturnStatusCode200() {
+        when(lrdBuildingLocationService.retrieveBuildingLocationByEpimsIds(anyList()))
             .thenReturn(new ArrayList<>());
         ResponseEntity<Object> responseEntity =
             lrdApiController.retrieveBuildingLocationDetailsByEpimsId("111");
-        verify(lrdBuildingLocationService, times(1)).retrieveBuildingLocationByEpimsId(anyList());
+        verify(lrdBuildingLocationService, times(1)).retrieveBuildingLocationByEpimsIds(anyList());
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
     public void testGetBuildingLocations_AllEpimmsIdGiven_ShouldReturnStatusCode200() {
-        when(lrdBuildingLocationService.retrieveBuildingLocationByEpimsId(anyList()))
+        when(lrdBuildingLocationService.getAllBuildingLocations())
             .thenReturn(new ArrayList<>());
         ResponseEntity<Object> responseEntity =
             lrdApiController.retrieveBuildingLocationDetailsByEpimsId("ALL");
@@ -57,7 +56,7 @@ public class LrdApiControllerGetBuildingLocationsTest {
 
     @Test
     public void testGetBuildingLocations_lowerAllEpimmsIdGiven_ShouldReturnStatusCode200() {
-        when(lrdBuildingLocationService.retrieveBuildingLocationByEpimsId(anyList()))
+        when(lrdBuildingLocationService.getAllBuildingLocations())
             .thenReturn(new ArrayList<>());
         ResponseEntity<Object> responseEntity =
             lrdApiController.retrieveBuildingLocationDetailsByEpimsId("all");
@@ -67,52 +66,83 @@ public class LrdApiControllerGetBuildingLocationsTest {
 
     @Test
     public void testGetBuildingLocations_EpimmsIdWithASpaceGiven_ShouldReturnStatusCode200() {
-        when(lrdBuildingLocationService.retrieveBuildingLocationByEpimsId(anyList()))
+        when(lrdBuildingLocationService.retrieveBuildingLocationByEpimsIds(anyList()))
             .thenReturn(new ArrayList<>());
         ResponseEntity<Object> responseEntity =
             lrdApiController.retrieveBuildingLocationDetailsByEpimsId(" 111 ");
-        verify(lrdBuildingLocationService, times(1)).retrieveBuildingLocationByEpimsId(anyList());
+        verify(lrdBuildingLocationService, times(1)).retrieveBuildingLocationByEpimsIds(anyList());
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 
     @Test
-    public void testGetBuildingLocations_NullEpimmsIdGiven_ShouldReturnStatusCode200() {
+    public void testGetBuildingLocations_ValidMultipleEpimmsIdGiven_ShouldReturnStatusCode200() {
+        when(lrdBuildingLocationService.retrieveBuildingLocationByEpimsIds(anyList()))
+            .thenReturn(new ArrayList<>());
         ResponseEntity<Object> responseEntity =
-            lrdApiController.retrieveBuildingLocationDetailsByEpimsId(null);
-
-        assertThat(responseEntity).isNotNull();
+            lrdApiController.retrieveBuildingLocationDetailsByEpimsId("111,qwerty, qwerty_123");
+        verify(lrdBuildingLocationService, times(1)).retrieveBuildingLocationByEpimsIds(anyList());
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        verify(lrdBuildingLocationService, times(0)).retrieveBuildingLocationByEpimsId(anyList());
     }
 
     @Test
-    public void testGetBuildingLocations_StringEpimmsIdGiven_ShouldReturnStatusCode200() {
+    public void testGetBuildingLocations_1Valid1InvalidEpimmsIdGiven_ShouldReturnStatusCode200() {
+        when(lrdBuildingLocationService.retrieveBuildingLocationByEpimsIds(anyList()))
+            .thenReturn(new ArrayList<>());
+        ResponseEntity<Object> responseEntity =
+            lrdApiController.retrieveBuildingLocationDetailsByEpimsId("!@£$%,qwerty");
+        verify(lrdBuildingLocationService, times(1)).retrieveBuildingLocationByEpimsIds(anyList());
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void testGetBuildingLocations_AllInvalidEpimmsIdGiven_ShouldReturnStatusCode400() {
+        ResponseEntity<Object> responseEntity =
+            lrdApiController.retrieveBuildingLocationDetailsByEpimsId("!@£$%,&*(");
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        verify(lrdBuildingLocationService, times(0)).getAllBuildingLocations();
+        verify(lrdBuildingLocationService, times(0))
+            .retrieveBuildingLocationByEpimsIds(anyList());
+    }
+
+    @Test(expected = InvalidRequestException.class)
+    public void testGetBuildingLocations_SingleInvalidEpimmsIdGiven_ShouldReturnStatusCode400() {
+        ResponseEntity<Object> responseEntity =
+            lrdApiController.retrieveBuildingLocationDetailsByEpimsId("abc 123");
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        verify(lrdBuildingLocationService, times(0)).getAllBuildingLocations();
+        verify(lrdBuildingLocationService, times(0))
+            .retrieveBuildingLocationByEpimsIds(anyList());
+    }
+
+    @Test
+    public void testGetBuildingLocations_StringEpimsIdGiven_ShouldReturnStatusCode200() {
         ResponseEntity<Object> responseEntity =
             lrdApiController.retrieveBuildingLocationDetailsByEpimsId("QWERTY");
 
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        verify(lrdBuildingLocationService, times(0)).retrieveBuildingLocationByEpimsId(anyList());
+        verify(lrdBuildingLocationService, times(1)).retrieveBuildingLocationByEpimsIds(anyList());
     }
 
     @Test
-    public void testGetBuildingLocations_AlphaNumericEpimmsIdGiven_ShouldReturnStatusCode200() {
+    public void testGetBuildingLocations_AlphaNumericEpimsIdGiven_ShouldReturnStatusCode200() {
         ResponseEntity<Object> responseEntity =
             lrdApiController.retrieveBuildingLocationDetailsByEpimsId("1234QWERTY");
 
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        verify(lrdBuildingLocationService, times(0)).retrieveBuildingLocationByEpimsId(anyList());
+        verify(lrdBuildingLocationService, times(1)).retrieveBuildingLocationByEpimsIds(anyList());
     }
 
     @Test(expected = InvalidRequestException.class)
-    public void testGetBuildingLocations_SpecialCharactersInEpimmsIdGiven_ShouldReturnStatusCode400() {
+    public void testGetBuildingLocations_OneSpecialCharEpimsIdGiven_ShouldReturnStatusCode400() {
         ResponseEntity<Object> responseEntity =
             lrdApiController.retrieveBuildingLocationDetailsByEpimsId("@£$%^");
 
         assertThat(responseEntity).isNotNull();
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
-        verify(lrdBuildingLocationService, times(0)).retrieveBuildingLocationByEpimsId(anyList());
+        verify(lrdBuildingLocationService, times(0)).getAllBuildingLocations();
+        verify(lrdBuildingLocationService, times(0)).retrieveBuildingLocationByEpimsIds(anyList());
     }
 
 }
--- src/test/java/uk/gov/hmcts/reform/lrdapi/service/impl/LrdBuildingLocationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/lrdapi/service/impl/LrdBuildingLocationServiceTest.java
@@ -21,7 +21,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -41,12 +40,12 @@ public class LrdBuildingLocationServiceTest {
     }
 
     @Test
-    public void test_RetrieveBuildingLocationsByEpimsID() {
+    public void test_RetrieveBuildingLocationsByEpimsIDs_OneIdPassed() {
 
         when(buildingLocationRepository.findByEpimmsIdIn(anyList())).thenReturn(prepareBuildingLocation());
 
         List<LrdBuildingLocationResponse> buildingLocations =
-            lrdBuildingLocationService.retrieveBuildingLocationByEpimsId(getListOfEpimIds());
+            lrdBuildingLocationService.retrieveBuildingLocationByEpimsIds(getListContainingOneEpimId());
 
         LrdBuildingLocationResponse buildingLocation = buildingLocations.get(0);
 
@@ -64,34 +63,84 @@ public class LrdBuildingLocationServiceTest {
         assertThat(buildingLocation.getAddress()).isEqualTo("address");
     }
 
+    @Test
+    public void test_RetrieveBuildingLocationsByEpimsIDs_MultipleIdsPassed() {
+
+        when(buildingLocationRepository.findByEpimmsIdIn(anyList())).thenReturn(prepareMultiBuildLocationResponse());
+        List<LrdBuildingLocationResponse> buildingLocations =
+            lrdBuildingLocationService.retrieveBuildingLocationByEpimsIds(getListContainingMultipleEpimIds());
+        verifyMultiResponse(buildingLocations);
+    }
+
+    @Test
+    public void testGetAllBuildingLocations() {
+        when(buildingLocationRepository.findAll()).thenReturn(prepareMultiBuildLocationResponse());
+        List<LrdBuildingLocationResponse> buildingLocations = lrdBuildingLocationService.getAllBuildingLocations();
+        verifyMultiResponse(buildingLocations);
+    }
+
     @Test(expected = ResourceNotFoundException.class)
     public void test_RetrieveBuildingLocations_NoBuildLocationFound() {
         when(buildingLocationRepository.findByEpimmsIdIn(anyList())).thenReturn(null);
-        lrdBuildingLocationService.retrieveBuildingLocationByEpimsId(getListOfEpimIds());
+        lrdBuildingLocationService.retrieveBuildingLocationByEpimsIds(getListContainingOneEpimId());
         verify(buildingLocationRepository.findByEpimmsIdIn(anyList()), times(1));
     }
 
-    private List<String> getListOfEpimIds() {
+    @Test(expected = ResourceNotFoundException.class)
+    public void test_GetAllBuildingLocations_NoBuildLocationFound() {
+        when(buildingLocationRepository.findAll()).thenReturn(null);
+        lrdBuildingLocationService.getAllBuildingLocations();
+        verify(buildingLocationRepository.findAll(), times(1));
+    }
+
+    private void verifyMultiResponse(List<LrdBuildingLocationResponse> buildingLocations) {
+        assertThat(buildingLocations.size()).isEqualTo(2);
+
+        buildingLocations.forEach(buildingLocation -> {
+            if(buildingLocation.getEpimmsId().equalsIgnoreCase("epimmsId")) {
+                assertThat(buildingLocation.getBuildingLocationId()).isEqualTo("buildingLocationId");
+                assertThat(buildingLocation.getEpimmsId()).isEqualTo("epimmsId");
+                assertThat(buildingLocation.getBuildingLocationName()).isEqualTo("buildingLocationName");
+                assertThat(buildingLocation.getBuildingLocationStatus()).isEqualTo("LIVE");
+                assertThat(buildingLocation.getArea()).isEqualTo("area");
+                assertThat(buildingLocation.getRegionId()).isEqualTo("regionId");
+                assertThat(buildingLocation.getRegion()).isEqualTo("region");
+                assertThat(buildingLocation.getClusterId()).isEqualTo("clusterId");
+                assertThat(buildingLocation.getClusterName()).isEqualTo("cluster name");
+                assertThat(buildingLocation.getCourtFinderUrl()).isEqualTo("courtFinderUrl");
+                assertThat(buildingLocation.getPostcode()).isEqualTo("postcode");
+                assertThat(buildingLocation.getAddress()).isEqualTo("address");
+            } else {
+                assertThat(buildingLocation.getBuildingLocationId()).isEqualTo("buildingLocationId_2");
+                assertThat(buildingLocation.getEpimmsId()).isEqualTo("epimmsId222");
+                assertThat(buildingLocation.getBuildingLocationName()).isEqualTo("buildingLocationName_2");
+                assertThat(buildingLocation.getBuildingLocationStatus()).isEqualTo("LIVE");
+                assertThat(buildingLocation.getArea()).isEqualTo("area 2");
+                assertThat(buildingLocation.getRegionId()).isEqualTo("regionId");
+                assertThat(buildingLocation.getRegion()).isEqualTo("region");
+                assertThat(buildingLocation.getClusterId()).isEqualTo("clusterId");
+                assertThat(buildingLocation.getClusterName()).isEqualTo("cluster name");
+                assertThat(buildingLocation.getCourtFinderUrl()).isEqualTo("courtFinderUrl 2");
+                assertThat(buildingLocation.getPostcode()).isEqualTo("postcode 2");
+                assertThat(buildingLocation.getAddress()).isEqualTo("address 2");
+            }
+        });
+    }
+
+    private List<String> getListContainingOneEpimId() {
         List<String> epimIdList = new ArrayList<>();
         epimIdList.add("1");
         return epimIdList;
     }
 
+    private List<String> getListContainingMultipleEpimIds() {
+        List<String> epimIdList = new ArrayList<>();
+        epimIdList.addAll(getListContainingOneEpimId());
+        epimIdList.add("2");
+        return epimIdList;
+    }
+
     private List<BuildingLocation> prepareBuildingLocation() {
-
-        BuildingLocationStatus status = new BuildingLocationStatus();
-        status.setStatus("LIVE");
-        status.setBuildingStatusId("1");
-
-        Region region = new Region();
-        region.setRegionId("regionId");
-        region.setDescription("region");
-
-        Cluster cluster = new Cluster();
-        cluster.setClusterId("clusterId");
-        cluster.setClusterName("cluster name");
-
-        LocalDateTime now = LocalDateTime.now();
 
         List<BuildingLocation> locations = new ArrayList<>();
 
@@ -99,18 +148,66 @@ public class LrdBuildingLocationServiceTest {
                          .epimmsId("epimmsId")
                          .buildingLocationId("buildingLocationId")
                          .buildingLocationName("buildingLocationName")
-                         .buildingLocationStatus(status)
-                         .region(region)
+                         .buildingLocationStatus(getBuildingLocationStatus())
+                         .region(getRegion())
                          .address("address")
-                         .cluster(cluster)
+                         .cluster(getCluster())
                          .courtFinderUrl("courtFinderUrl")
                          .area("area")
                          .postcode("postcode")
-                         .created(now)
-                         .lastUpdated(now)
+                         .created(getCurrentTime())
+                         .lastUpdated(getCurrentTime())
                          .build());
 
         return locations;
     }
+
+    private List<BuildingLocation> prepareMultiBuildLocationResponse() {
+        List<BuildingLocation> locations = new ArrayList<>();
+        locations.addAll(prepareBuildingLocation());
+        locations.add(BuildingLocation.builder()
+                          .epimmsId("epimmsId222")
+                          .buildingLocationId("buildingLocationId_2")
+                          .buildingLocationName("buildingLocationName_2")
+                          .buildingLocationStatus(getBuildingLocationStatus())
+                          .region(getRegion())
+                          .address("address 2")
+                          .cluster(getCluster())
+                          .courtFinderUrl("courtFinderUrl 2")
+                          .area("area 2")
+                          .postcode("postcode 2")
+                          .created(getCurrentTime())
+                          .lastUpdated(getCurrentTime())
+                          .build());
+
+        return locations;
+    }
+
+    private LocalDateTime getCurrentTime() {
+        return LocalDateTime.now();
+    }
+
+    private Cluster getCluster() {
+        Cluster cluster = new Cluster();
+        cluster.setClusterId("clusterId");
+        cluster.setClusterName("cluster name");
+        return cluster;
+    }
+
+    private Region getRegion() {
+        Region region = new Region();
+        region.setRegionId("regionId");
+        region.setDescription("region");
+        return region;
+    }
+
+    private BuildingLocationStatus getBuildingLocationStatus() {
+        BuildingLocationStatus status = new BuildingLocationStatus();
+        status.setStatus("LIVE");
+        status.setBuildingStatusId("1");
+        return status;
+    }
+
+
 
 }
