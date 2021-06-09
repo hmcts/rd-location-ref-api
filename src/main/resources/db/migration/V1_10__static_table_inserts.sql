1eb218cb51ebf0cfbe7ee0e428e7dc582f886d47
AdilOoze <adil.oozeerally@hmcts.net>
2021-06-09 11:15:43 +0100

    RDCC-2785: making welsh column nullable, removing ' ' inserts

--- src/main/resources/db/migration/V1_10__static_table_inserts.sql
+++ src/main/resources/db/migration/V1_10__static_table_inserts.sql
@@ -1,208 +1,169 @@
+alter table court_location_category alter column welsh_court_location_category drop not null;
+
 insert into court_location_category(
     court_location_category_id,
     court_location_category,
-    welsh_court_location_category,
     created_time
     )
 values(
     '1',
     'Admin Court',
-    ' ',
     now()),
     (
     '2',
     'Admiralty and Commercial Court',
-    ' ',
     now()),
     (
     '3',
     'Agricultural Land and Drainage Tribunal',
-    ' ',
     now()),
     (
     '4',
     'Asylum Support Appeals',
-    ' ',
     now()),
     (
     '5',
     'Bankruptcy Court (High Court)',
-    ' ',
     now()),
     (
     '6',
     'Business and Property Court',
-    ' ',
     now()),
     (
     '7',
     'Central London County Court',
-    ' ',
     now()),
     (
     '8',
     'Central London County Court (Bankruptcy)',
-    ' ',
     now()),
     (
     '9',
     'Chancery Division',
-    ' ',
     now()),
     (
     '10',
     'County Court',
-    ' ',
     now()),
     (
     '11',
     'Court of Appeal Civil Division',
-    ' ',
     now()),
     (
     '12',
     'Court of Appeal Criminal Division',
-    ' ',
     now()),
     (
     '13',
     'Court of Protection',
-    ' ',
     now()),
     (
     '14',
     'Criminal Injuries Compensation',
-    ' ',
     now()),
     (
     '15',
     'Crown Court',
-    ' ',
     now()),
     (
     '16',
     'Employment Appeal Tribunal (England and Wales)',
-    ' ',
     now()),
     (
     '17',
     'Employment Tribunal',
-    ' ',
     now()),
     (
     '18',
     'Family Court',
-    ' ',
     now()),
     (
     '19',
     'Gangmasters Licensing Appeals',
-    ' ',
     now()),
     (
     '20',
     'General Regulatory Chamber',
-    ' ',
     now()),
     (
     '21',
     'High Court',
-    ' ',
     now()),
     (
     '22',
     'Housing Centre',
-    ' ',
     now()),
     (
     '23',
     'Immigration and Asylum Tribunal',
-    ' ',
     now()),
     (
     '24',
     'Lands Tribunal',
-    ' ',
     now()),
     (
     '25',
     'Magistrates Court',
-    ' ',
     now()),
     (
     '26',
     'Patents Court',
-    ' ',
     now()),
     (
     '27',
     'Probate',
-    ' ',
     now()),
     (
     '28',
     'Queen''s Bench Division (General)',
-    ' ',
     now()),
     (
     '29',
     'Queen''s Bench Division Administrative Court',
-    ' ',
     now()),
     (
     '30',
     'Residential Property Tribunal',
-    ' ',
     now()),
     (
     '31',
     'Social Security and Child Support Tribunal',
-    ' ',
     now()),
     (
     '32',
     'Special Immigration Appeals Commission',
-    ' ',
     now()),
     (
     '33',
     'Tax Tribunal',
-    ' ',
     now()),
     (
     '34',
     'Technology & Construction Court',
-    ' ',
     now()),
     (
     '35',
     'Tribunals',
-    ' ',
     now()),
     (
     '36',
     'Upper Tier Immigration and Asylum Tribunal',
-    ' ',
     now()),
     (
     '37',
     'Upper Tribunal (Administrative Appeals Chamber)',
-    ' ',
     now()),
     (
     '38',
     'Upper Tribunal (Lands Chamber)',
-    ' ',
     now()),
     (
     '39',
     'Upper Tribunal (Tax and Chancery Chamber)',
-    ' ',
     now()),
     (
     '40',
     'War Pensions & Armed Forces Compensation Chamber',
-    ' ',
     now())
 ;
 
