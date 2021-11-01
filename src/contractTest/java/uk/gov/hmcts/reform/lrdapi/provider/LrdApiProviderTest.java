b7366288b541b9927e1aab9b86b9fecc51b4ff29
ManishParyani <manish.paryani@hmcts.net>
2021-11-01 08:05:54 +0000

    updated pact test case

--- src/contractTest/java/uk/gov/hmcts/reform/lrdapi/provider/LrdApiProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/lrdapi/provider/LrdApiProviderTest.java
@@ -174,6 +174,9 @@ public class LrdApiProviderTest {
             .postcode("AB11 6LT")
             .courtAddress("AB1, 48 HUNTLY STREET, ABERDEEN")
             .courtVenueId(1L)
+            .venueName("venueName")
+            .isCaseManagementLocation("Y")
+            .isHearingLocation("Y")
             .build();
 
         Set<CourtVenue> courtVenues = new HashSet<>();
@@ -287,6 +290,9 @@ public class LrdApiProviderTest {
             .dxAddress("dxAddress1")
             .courtStatus("Open")
             .courtName("courtName1")
+            .venueName("venueName")
+            .isCaseManagementLocation("Y")
+            .isHearingLocation("Y")
             .build();
     }
 
@@ -307,6 +313,9 @@ public class LrdApiProviderTest {
             .dxAddress("dxAddress")
             .courtStatus("Open")
             .courtName("courtName")
+            .venueName("venueName")
+            .isCaseManagementLocation("Y")
+            .isHearingLocation("Y")
             .build();
 
         CourtVenue secondCourtVenue = CourtVenue.builder()
@@ -325,6 +334,9 @@ public class LrdApiProviderTest {
             .dxAddress("dxAddress")
             .courtStatus("Closed")
             .courtName("courtName")
+            .venueName("venueName")
+            .isCaseManagementLocation("Y")
+            .isHearingLocation("Y")
             .build();
 
         return List.of(firstCourtVenue, secondCourtVenue);
