dd882d04b7a201b31f759dc8c5105cbcf4c773c4
ishratt <tunazzina.ishrat@hmcts.net>
2021-11-26 17:39:26 +0000

    RDCC-3620: replaced deprecated methods and minor refactoring

--- src/functionalTest/java/uk/gov/hmcts/reform/lrdapi/serenity5/extension/SerenityPageExtension.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/lrdapi/serenity5/extension/SerenityPageExtension.java
@@ -20,7 +20,6 @@ import java.util.function.Consumer;
 import static java.util.Optional.empty;
 import static java.util.Optional.of;
 import static java.util.Optional.ofNullable;
-import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getPages;
 import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.initializeFieldsIn;
 
 public class SerenityPageExtension implements BeforeEachCallback {
@@ -103,7 +102,7 @@ public class SerenityPageExtension implements BeforeEachCallback {
     }
 
     private void injectPageObjectIntoTest(final Object testClass) {
-        new PageObjectDependencyInjector(getPages()).injectDependenciesInto(testClass);
+        new PageObjectDependencyInjector().injectDependenciesInto(testClass);
     }
 
     public static class ExplicitWebDriverConfiguration {
