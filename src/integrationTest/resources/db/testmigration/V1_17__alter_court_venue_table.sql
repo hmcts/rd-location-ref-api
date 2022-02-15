8bd37b9d9b5d2374eb4a52fd956f25993d5316eb
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-02-15 17:51:25 +0000

    Changes made RDCC-3849: Changes made to resolve code merge issues

--- src/integrationTest/resources/db/testmigration/V1_17__alter_court_venue_table.sql
+++ src/integrationTest/resources/db/testmigration/V1_17__alter_court_venue_table.sql
@@ -1,8 +1,4 @@
-ALTER TABLE court_venue ADD COLUMN welsh_venue_name varchar(256);
-ALTER TABLE court_venue ADD COLUMN is_temporary_location varchar(1);
-ALTER TABLE court_venue ADD COLUMN is_nightingale_court varchar(1);
-ALTER TABLE court_venue ADD COLUMN location_type varchar(16);
-ALTER TABLE court_venue ADD COLUMN parent_location varchar(16);
+
 
 update court_venue
 set welsh_venue_name = 'testVenue', is_temporary_location = 'N', is_nightingale_court = 'N', location_type = 'Court', parent_location = '366559'
