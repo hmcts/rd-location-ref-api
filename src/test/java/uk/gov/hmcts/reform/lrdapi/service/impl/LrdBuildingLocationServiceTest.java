3aac511767f86cf4cdb207252bc8a080db394238
ishratt <tunazzina.ishrat@hmcts.net>
2022-03-01 11:52:42 +0000

    RDCC-4073: minor refactoring

--- src/test/java/uk/gov/hmcts/reform/lrdapi/service/impl/LrdBuildingLocationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/lrdapi/service/impl/LrdBuildingLocationServiceTest.java
@@ -113,6 +113,13 @@ class LrdBuildingLocationServiceTest {
 
     @Test
     @SuppressWarnings("unchecked")
+    void testGetBuildingLocationsByInvalidClusterIdParam() {
+        assertThrows(InvalidRequestException.class, () -> lrdBuildingLocationService
+            .retrieveBuildingLocationDetails("", "", "", "1,2"));
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
     void testGetBuildingLocationsByClusterId() {
         when(buildingLocationRepository.findByClusterId(anyString())).thenReturn(prepareBuildingLocation());
         var buildingLocations = (List<LrdBuildingLocationResponse>) lrdBuildingLocationService
