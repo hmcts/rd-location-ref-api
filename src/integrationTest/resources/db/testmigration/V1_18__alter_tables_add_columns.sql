221d8b850eddf667a3e4f69409d2e9e78acceb19
Sourav Roy <sourav.roy@hmcts.net>
2022-03-07 11:03:43 +0000

    new fields added to CourtVenue.java

--- src/integrationTest/resources/db/testmigration/V1_18__alter_tables_add_columns.sql
+++ src/integrationTest/resources/db/testmigration/V1_18__alter_tables_add_columns.sql
@@ -8,3 +8,8 @@ ALTER TABLE court_venue ADD COLUMN fact_url VARCHAR(1024);
 ALTER TABLE court_venue ADD COLUMN mrd_created_time TIMESTAMP;
 ALTER TABLE court_venue ADD COLUMN mrd_updated_time TIMESTAMP;
 ALTER TABLE court_venue ADD COLUMN mrd_deleted_time TIMESTAMP;
+
+update court_venue
+set welsh_court_name = 'welshCourtName1', uprn = '1234', venue_ou_code = '87675', mrd_building_location_id = '8686',
+mrd_venue_id = '765', service_url = 'https://serviceurl.com', fact_url = 'https://facturl.com'
+where court_venue_id = 1 and epimms_id = '123456789'
