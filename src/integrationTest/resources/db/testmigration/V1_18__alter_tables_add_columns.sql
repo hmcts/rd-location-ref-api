6b464b528eeec685176fbeb721232467531ecc90
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-03-15 07:43:11 +0000

    RDCC:4200:Resolved merge conflicts

--- src/integrationTest/resources/db/testmigration/V1_18__alter_tables_add_columns.sql
+++ src/integrationTest/resources/db/testmigration/V1_18__alter_tables_add_columns.sql
@@ -9,10 +9,7 @@ ALTER TABLE court_venue ADD COLUMN mrd_created_time TIMESTAMP;
 ALTER TABLE court_venue ADD COLUMN mrd_updated_time TIMESTAMP;
 ALTER TABLE court_venue ADD COLUMN mrd_deleted_time TIMESTAMP;
 
-<<<<<<< HEAD
 
-=======
->>>>>>> master
 ALTER TABLE building_location ADD COLUMN welsh_building_location_name VARCHAR(256);
 ALTER TABLE building_location ADD COLUMN welsh_address VARCHAR(512);
 ALTER TABLE building_location ADD COLUMN uprn VARCHAR(16);
