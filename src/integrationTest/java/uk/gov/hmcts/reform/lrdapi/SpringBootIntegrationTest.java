2d0d8a0908736c5b5cd287a16af44751fec1469c
m_659064 <vilas.shelke@HMCTS.NET>
2023-01-24 12:26:49 +0000

    1. Integration Test update

--- src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/SpringBootIntegrationTest.java
@@ -3,14 +3,12 @@ package uk.gov.hmcts.reform.lrdapi;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = LrdApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {"spring.config.location=classpath:application-test.yml"})
-@AutoConfigureWireMock(port = 0)
 public abstract class SpringBootIntegrationTest {
 
     @LocalServerPort
