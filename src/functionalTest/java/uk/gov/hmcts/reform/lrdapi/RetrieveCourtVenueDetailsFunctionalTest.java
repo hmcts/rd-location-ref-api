a1f26e75f275eed73992c551c36c479118f1d93b
vasthav-patta <112877261+vasthav-patta@users.noreply.github.com>
2023-02-21 17:38:16 +0000

    funtional test fix

--- src/functionalTest/java/uk/gov/hmcts/reform/lrdapi/RetrieveCourtVenueDetailsFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/lrdapi/RetrieveCourtVenueDetailsFunctionalTest.java
@@ -105,7 +105,7 @@ class RetrieveCourtVenueDetailsFunctionalTest extends AuthorizationFunctionalTes
     @ToggleEnable(mapKey = mapKey, withFeature = true)
     void shouldRetrieveCourtVenues_For_A_Epimms_Id_With_CourtType_StatusCode_200() {
         final var response = (LrdCourtVenueResponse[])
-            lrdApiClient.retrieveResponseForGivenRequest(HttpStatus.OK, "?epimms_id=381649&court_type_id=25",
+            lrdApiClient.retrieveResponseForGivenRequest(HttpStatus.OK, "?epimms_id=219164&court_type_id=23",
                                                          LrdCourtVenueResponse[].class,
                                                          path
             );
