201d685b7e52de1f0318282526f68241788c7a2f
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-02-23 14:01:54 +0000

    [RDCC-6414]-Added Functional Test cases for accept CCDCaseType as List

--- src/functionalTest/java/uk/gov/hmcts/reform/lrdapi/RetrieveOrgServiceDetailsFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/lrdapi/RetrieveOrgServiceDetailsFunctionalTest.java
@@ -52,7 +52,7 @@ class RetrieveOrgServiceDetailsFunctionalTest extends AuthorizationFunctionalTes
                 "?ccdCaseType=MoneyClaimCase"
             );
 
-        assertEquals(1, responses.size());
+        assertEquals(2, responses.size());
         var ccdCaseTypes = responses
             .stream()
             .flatMap(service -> service.getCcdCaseTypes().stream())
@@ -64,12 +64,37 @@ class RetrieveOrgServiceDetailsFunctionalTest extends AuthorizationFunctionalTes
     @SuppressWarnings("unchecked")
     @Test
     @ToggleEnable(mapKey = mapKey, withFeature = true)
+    void returnsOrgServiceDetailsByCcdServiceNameWithStatusCode_200() {
+        List<LrdOrgInfoServiceResponse> responses = (List<LrdOrgInfoServiceResponse>)
+            lrdApiClient.retrieveOrgServiceInfo(
+                HttpStatus.OK,
+                "?ccdServiceNames=cMc"
+            );
+        assertEquals(2, responses.size());
+        assertTrue(responses.stream().allMatch(service -> service.getCcdServiceName().equalsIgnoreCase("cMc")));
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    @ToggleEnable(mapKey = mapKey, withFeature = true)
     void returnsOrgServiceDetailsByDefaultAll_200() throws JsonProcessingException {
 
         List<LrdOrgInfoServiceResponse> responses = (List<LrdOrgInfoServiceResponse>)
             lrdApiClient.retrieveOrgServiceInfo(HttpStatus.OK, "");
 
         assertThat(responses).isNotEmpty().hasSize(45);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    @ToggleEnable(mapKey = mapKey, withFeature = true)
+    void returnsOrgServiceDetailsByServiceNameStatusCode_200() throws JsonProcessingException {
+        //ServiceName is generated from the ServiceToCcdCaseTypeAssoc
+        List<LrdOrgInfoServiceResponse> responses = (List<LrdOrgInfoServiceResponse>)
+            lrdApiClient.retrieveOrgServiceInfo(HttpStatus.OK, "?serviceCode=AAA6");
+        assertEquals(1, responses.size());
+        assertTrue(responses.stream().allMatch(service -> service.getServiceCode().equals("AAA6")));
+        assertTrue(responses.stream().allMatch(service -> service.getCcdServiceName().equals("CMC")));
     }
 
     @Test
@@ -102,11 +127,11 @@ class RetrieveOrgServiceDetailsFunctionalTest extends AuthorizationFunctionalTes
             assertThat(response.getBusinessArea()).isEqualToIgnoringCase("Civil, Family and Tribunals");
             assertThat(response.getOrgUnit()).isEqualToIgnoringCase("HMCTS");
             assertNotNull(response.getCcdServiceName());
-            assertEquals("AAA6", response.getServiceCode());
+            assertNotNull(response.getServiceCode());
             assertThat(response.getJurisdiction()).isEqualToIgnoringCase("Civil");
             assertNotNull(response.getLastUpdate());
-            assertThat(response.getServiceDescription()).isEqualToIgnoringCase("Specified Money Claims");
-            assertThat(response.getServiceShortDescription()).isEqualToIgnoringCase("Specified Money Claims");
+            assertNotNull(response.getServiceDescription());
+            assertNotNull(response.getServiceShortDescription());
             assertThat(response.getSubBusinessArea()).isEqualToIgnoringCase("Civil and Family");
             assertThat(response.getCcdCaseTypes().size()).isPositive();
 
