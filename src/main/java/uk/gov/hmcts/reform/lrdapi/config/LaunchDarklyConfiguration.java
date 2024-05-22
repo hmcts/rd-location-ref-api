e95337c766db28dea353aed0734989725c60afce
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2024-05-22 17:49:32 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/lrdapi/config/LaunchDarklyConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/lrdapi/config/LaunchDarklyConfiguration.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.lrdapi.config;
 import com.launchdarkly.sdk.server.LDClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
