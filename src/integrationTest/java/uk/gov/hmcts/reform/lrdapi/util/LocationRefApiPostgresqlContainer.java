8e90f984b5e9d9d0d9d7ca699c5794161b33ad62
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-07-04 15:39:54 +0100

    Fix Docker image issue and enable enablePendingPacts

--- src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/util/LocationRefApiPostgresqlContainer.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/util/LocationRefApiPostgresqlContainer.java
@@ -2,9 +2,7 @@ package uk.gov.hmcts.reform.lrdapi.util;
 
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 public class LocationRefApiPostgresqlContainer extends PostgreSQLContainer<LocationRefApiPostgresqlContainer> {
     private static final String IMAGE_VERSION = "postgres:11.1";
 
