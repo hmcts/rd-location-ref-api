5f3d93d33fbe72c0b8b71ceb36a605725613dea0
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-09-25 11:59:16 +0100

    DTSRD-3066: address court venu unique queue

--- src/integrationTest/resources/db/testmigration/V1_26__insert_court_venues.sql
+++ src/integrationTest/resources/db/testmigration/V1_26__insert_court_venues.sql
@@ -25,5 +25,5 @@ values(
   now());
 
 insert into court_venue (court_venue_id, epimms_id, site_name, created_time, updated_time, region_id, court_type_id, cluster_id, open_for_public, court_address, postcode, phone_number, closed_date, court_location_code, dx_address, welsh_site_name, welsh_court_address, court_status, court_open_date, court_name, venue_name, is_case_management_location, is_hearing_location, external_short_name)
-values(13, '123457','Aberdeen Tribunal Hearing Centre 7',	'2021-07-05 09:50:46.269032','2021-07-05 09:50:46.269032','2','17', '2',true,'AB7, 54 HUNTLY STREET, ABERDEEN','AB11 1TY',null,null,null,null,null,null,'Open', null,'ABERDEEN TRIBUNAL HEARING CENTRE 7',null,null,null, 'Aberdeen Tribunal External');
+values(13, '123461','Aberdeen Tribunal Hearing Centre 7',	'2021-07-05 09:50:46.269032','2021-07-05 09:50:46.269032','2','17', '2',true,'AB7, 54 HUNTLY STREET, ABERDEEN','AB11 1TY',null,null,null,null,null,null,'Open', null,'ABERDEEN TRIBUNAL HEARING CENTRE 7',null,null,null, 'Aberdeen Tribunal External');
 commit;
