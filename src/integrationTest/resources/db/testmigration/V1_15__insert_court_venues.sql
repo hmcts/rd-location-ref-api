68d54ab9807795a1dd3ccc8637f414875c6029c0
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-09-25 14:37:08 +0100

    DTSRD-3066: Fix building location primary key issue

--- src/integrationTest/resources/db/testmigration/V1_15__insert_court_venues.sql
+++ src/integrationTest/resources/db/testmigration/V1_15__insert_court_venues.sql
@@ -48,6 +48,32 @@ values(
   '4 Street, London',
   now());
 
+insert into
+  building_location(
+    building_location_id,
+    region_id,
+    building_location_status,
+    cluster_id,
+    epimms_id,
+    building_location_name,
+    area,
+    court_finder_url,
+    postcode,
+    address,
+    updated_time)
+values(
+  '22041999',
+  '2',
+  'Open',
+  '01234',
+  '123457',
+  'Building Location 4',
+  'Area D',
+  'Court Finder URL 4',
+  'EC2A 3AQ',
+  '4 Street, London',
+  now());
+
 insert into court_venue (court_venue_id, epimms_id, site_name, created_time, updated_time, region_id, court_type_id, cluster_id, open_for_public, court_address, postcode, phone_number, closed_date, court_location_code, dx_address, welsh_site_name, welsh_court_address, court_status, court_open_date, court_name, venue_name, is_case_management_location, is_hearing_location, welsh_venue_name, is_temporary_location, is_nightingale_court, location_type, parent_location)
 values(1, '123456787','Aberdeen Tribunal Hearing Centre 1',	'2021-07-05 09:50:46.269032','2021-07-05 09:50:46.269032','1','23', null,true,'AB1, 48 HUNTLY STREET, ABERDEEN','AB11 6LT',null,null,null,null,null,null,'Open', null,'ABERDEEN TRIBUNAL HEARING CENTRE 1','venueName1','Y','Y','testVenue','N','N','Court','366559');
 insert into court_venue (court_venue_id, epimms_id, site_name, created_time, updated_time, region_id, court_type_id, cluster_id, open_for_public, court_address, postcode, phone_number, closed_date, court_location_code, dx_address, welsh_site_name, welsh_court_address, court_status, court_open_date, court_name, venue_name, is_case_management_location, is_hearing_location)
