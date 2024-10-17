0348cf42d7f36f5959fa7b671eebd32282f7fcfa
Mike Brown <michael.brown@hmcts.net>
2024-10-17 14:28:51 +0100

    DTSRD-3562 Swagger Publishing

--- src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/config/SwaggerPublisherTest.java
@@ -47,7 +47,7 @@ class SwaggerPublisherTest extends LrdAuthorizationEnabledIntegrationTest {
             .getResponse()
             .getContentAsByteArray();
 
-        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/openapi-specs.json"))) {
             outputStream.write(specs);
         }
 
