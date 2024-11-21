675c510a05cda437e571d06a13e529701acc8335
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2024-11-21 15:11:06 +0000

    rename constraints in recreated tables

--- src/main/resources/db/migration/V1_29__spring-batch-schema-recreate.sql
+++ src/main/resources/db/migration/V1_29__spring-batch-schema-recreate.sql
@@ -1,23 +1,23 @@
-alter table locrefdata.BATCH_JOB_INSTANCE drop constraint if exists JOB_INST_UN;
-alter table locrefdata.BATCH_JOB_EXECUTION drop constraint if exists JOB_INST_EXEC_FK;
-alter table locrefdata.BATCH_JOB_EXECUTION_PARAMS drop constraint if exists JOB_EXEC_PARAMS_FK;
-alter table locrefdata.BATCH_STEP_EXECUTION drop constraint if exists JOB_EXEC_STEP_FK;
-alter table locrefdata.BATCH_STEP_EXECUTION_CONTEXT drop constraint if exists STEP_EXEC_CTX_FK;
-alter table locrefdata.BATCH_JOB_EXECUTION_CONTEXT drop constraint if exists JOB_EXEC_CTX_FK;
+drop table if exists batch_step_execution_context_archived;
+drop table if exists batch_job_execution_context_archived;
+drop table if exists batch_step_execution_archived;
+drop table if exists batch_job_execution_params_archived;
+drop table if exists batch_job_execution_archived;
+drop table if exists batch_job_instance_archived;
 
-drop table if exists locrefdata.batch_step_execution_context_archived;
-drop table if exists locrefdata.batch_job_execution_context_archived;
-drop table if exists locrefdata.batch_step_execution_archived;
-drop table if exists locrefdata.batch_job_execution_params_archived;
-drop table if exists locrefdata.batch_job_execution_archived;
-drop table if exists locrefdata.batch_job_instance_archived;
+alter table if exists batch_step_execution_context rename to batch_step_execution_context_archived;
+alter table if exists batch_job_execution_context rename to batch_job_execution_context_archived;
+alter table if exists batch_step_execution rename to batch_step_execution_archived;
+alter table if exists batch_job_execution_params rename to batch_job_execution_params_archived;
+alter table if exists batch_job_execution rename to batch_job_execution_archived;
+alter table if exists batch_job_instance rename to batch_job_instance_archived;
 
-alter table if exists locrefdata.batch_step_execution_context rename to batch_step_execution_context_archived;
-alter table if exists locrefdata.batch_job_execution_context rename to batch_job_execution_context_archived;
-alter table if exists locrefdata.batch_step_execution rename to batch_step_execution_archived;
-alter table if exists locrefdata.batch_job_execution_params rename to batch_job_execution_params_archived;
-alter table if exists locrefdata.batch_job_execution rename to batch_job_execution_archived;
-alter table if exists locrefdata.batch_job_instance rename to batch_job_instance_archived;
+alter table if exists batch_job_instance_archived rename constraint JOB_INST_UN to JOB_INST_UN_A;
+alter table if exists batch_job_execution_archived rename constraint JOB_INST_EXEC_FK to JOB_INST_EXEC_FK_A;
+alter table if exists batch_job_execution_params_archived rename constraint JOB_EXEC_PARAMS_FK to JOB_EXEC_PARAMS_FK_A;
+alter table if exists batch_step_execution_archived rename constraint JOB_EXEC_STEP_FK to JOB_EXEC_STEP_FK_A;
+alter table if exists batch_step_execution_context_archived rename constraint STEP_EXEC_CTX_FK to STEP_EXEC_CTX_FK_A;
+alter table if exists batch_job_execution_context_archived rename constraint JOB_EXEC_CTX_FK to JOB_EXEC_CTX_FK_A;
 
 CREATE TABLE BATCH_JOB_INSTANCE  (
 	JOB_INSTANCE_ID BIGINT  NOT NULL PRIMARY KEY ,
