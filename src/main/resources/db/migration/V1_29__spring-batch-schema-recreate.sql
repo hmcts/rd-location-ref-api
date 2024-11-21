51b6ca35b75bfb3fc6122e3c56732679f9058b89
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2024-11-21 14:34:16 +0000

    removal of constraints and pre-existing tables

--- src/main/resources/db/migration/V1_29__spring-batch-schema-recreate.sql
+++ src/main/resources/db/migration/V1_29__spring-batch-schema-recreate.sql
@@ -1,3 +1,17 @@
+alter table locrefdata.BATCH_JOB_INSTANCE drop constraint if exists JOB_INST_UN;
+alter table locrefdata.BATCH_JOB_EXECUTION drop constraint if exists JOB_INST_EXEC_FK;
+alter table locrefdata.BATCH_JOB_EXECUTION_PARAMS drop constraint if exists JOB_EXEC_PARAMS_FK;
+alter table locrefdata.BATCH_STEP_EXECUTION drop constraint if exists JOB_EXEC_STEP_FK;
+alter table locrefdata.BATCH_STEP_EXECUTION_CONTEXT drop constraint if exists STEP_EXEC_CTX_FK;
+alter table locrefdata.BATCH_JOB_EXECUTION_CONTEXT drop constraint if exists JOB_EXEC_CTX_FK;
+
+drop table if exists batch_step_execution_context_archived;
+drop table if exists batch_job_execution_context_archived;
+drop table if exists batch_step_execution_archived;
+drop table if exists batch_job_execution_params_archived;
+drop table if exists batch_job_execution_archived;
+drop table if exists batch_job_instance_archived;
+
 alter table if exists locrefdata.batch_step_execution_context rename to batch_step_execution_context_archived;
 alter table if exists locrefdata.batch_job_execution_context rename to batch_job_execution_context_archived;
 alter table if exists locrefdata.batch_step_execution rename to batch_step_execution_archived;
