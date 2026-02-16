8aa63c55a1c0c0af35b25b79d25494b11ad663e2
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-02-16 12:21:26 +0000

    DTSRD-4052 . Adding extra column to court venue table

--- src/integrationTest/resources/db/testmigration/V1_33__update_court_venue_service_code.sql
+++ src/integrationTest/resources/db/testmigration/V1_33__update_court_venue_service_code.sql
@@ -1,19 +1,20 @@
 -- Update court venue test data with service_code values
-UPDATE court_venue SET service_code = 'AAA2' WHERE court_venue_id = 1;
-UPDATE court_venue SET service_code = 'AAA3' WHERE court_venue_id = 2;
 UPDATE court_venue SET service_code = 'AAA6' WHERE court_venue_id = 3;
-UPDATE court_venue SET service_code = 'ABA4' WHERE court_venue_id = 4;
-UPDATE court_venue SET service_code = 'AAA2' WHERE court_venue_id = 5;
-UPDATE court_venue SET service_code = 'AAA3' WHERE court_venue_id = 6;
-UPDATE court_venue SET service_code = 'AAA6' WHERE court_venue_id = 7;
-UPDATE court_venue SET service_code = 'ABA4' WHERE court_venue_id = 8;
-UPDATE court_venue SET service_code = 'AAA2' WHERE court_venue_id = 9;
-UPDATE court_venue SET service_code = 'AAA3' WHERE court_venue_id = 10;
-UPDATE court_venue SET service_code = 'AAA6' WHERE court_venue_id = 11;
-UPDATE court_venue SET service_code = 'ABA4' WHERE court_venue_id = 12;
 UPDATE court_venue SET service_code = 'AAA2' WHERE court_venue_id = 13;
 UPDATE court_venue SET service_code = 'AAA3' WHERE court_venue_id = 14;
-UPDATE court_venue SET service_code = 'AAA6' WHERE court_venue_id = 15;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 1;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 2;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 3;
+UPDATE court_venue SET service_code = 'ABA4' WHERE court_venue_id = 4;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 5;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 6;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 7;
+UPDATE court_venue SET service_code = 'ABA4' WHERE court_venue_id = 8;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 9;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 10;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 11;
+UPDATE court_venue SET service_code = 'ABA4' WHERE court_venue_id = 12;
+UPDATE court_venue SET service_code = 'AAA4' WHERE court_venue_id = 15;
 
 COMMIT;
 
