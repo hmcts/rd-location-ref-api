7682f243bbe4b422507823d385e6aa811e41d9a5
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-02-19 14:29:16 +0000

    DTSRD-5700.LRD Court Venue - Updates to retrieveCourtVenuesByServiceCode API for new model

--- src/main/resources/db/migration/V1_32__alter_court_venue_add_service_code_composite_pk.sql
+++ src/main/resources/db/migration/V1_32__alter_court_venue_add_service_code_composite_pk.sql
@@ -10,5 +10,3 @@ ALTER TABLE court_venue ADD CONSTRAINT court_location_unique UNIQUE (epimms_id, 
 -- Add foreign key constraint for service_code
 ALTER TABLE court_venue ADD CONSTRAINT court_venue_service_code_fk FOREIGN KEY (service_code) REFERENCES SERVICE (service_code);
 
-insert into court_venue (court_venue_id, epimms_id, site_name, created_time, updated_time, region_id, court_type_id, cluster_id, open_for_public, court_address, postcode, phone_number, closed_date, court_location_code, dx_address, welsh_site_name, welsh_court_address, court_status, court_open_date, court_name, is_case_management_location, is_hearing_location, is_temporary_location, location_type,service_code)
-values(2, '815833','Aberdeen Tribunal Hearing Centre 11',	'2021-07-05 09:50:46.269032','2021-07-05 09:50:46.269032','7','1', '8',true,'AB10, 57 HUNTLY STREET, ABERDEEN','AB11 5QA',null,null,null,null,null,null,'Open', null,'ABERDEEN TRIBUNAL HEARING CENTRE 11', 'Y', 'Y', 'N', 'CTSC','AAA6');
