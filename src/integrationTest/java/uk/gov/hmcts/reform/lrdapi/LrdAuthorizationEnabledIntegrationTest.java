81bc89711d103c518a1d3c0bf818a9bf37f1e42b
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-04-07 17:22:19 +0100

    DTSRD-5698-depricate court_type parameter

--- src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/LrdAuthorizationEnabledIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/lrdapi/LrdAuthorizationEnabledIntegrationTest.java
@@ -50,7 +50,7 @@ import static uk.gov.hmcts.reform.lrdapi.util.JwtTokenUtil.decodeJwtToken;
 import static uk.gov.hmcts.reform.lrdapi.util.JwtTokenUtil.getUserIdAndRoleFromToken;
 
 @Configuration
-@TestPropertySource(properties = {"S2S_URL=http://127.0.0.1:8990", "IDAM_URL:http://127.0.0.1:5000"})
+@TestPropertySource(properties = {"S2S_URL=http://127.0.0.1:8990", "IDAM_URL=http://127.0.0.1:5000"})
 @DirtiesContext
 public abstract class LrdAuthorizationEnabledIntegrationTest extends SpringBootIntegrationTest {
 
@@ -185,4 +185,3 @@ public abstract class LrdAuthorizationEnabledIntegrationTest extends SpringBootI
 
 
 }
-
