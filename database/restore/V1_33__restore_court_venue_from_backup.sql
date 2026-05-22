bd66d27bd9960859ce9faccd825792595ae98d5a
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-05-22 10:02:46 +0100

    fixing review comments .changed back up table name , removed court_district_family_jurisdiction_assoc  and court_district_civil_jurisdiction_assoc restore of FK to keep it clean

--- database/restore/V1_33__restore_court_venue_from_backup.sql
+++ database/restore/V1_33__restore_court_venue_from_backup.sql
@@ -2,16 +2,13 @@
 SET search_path TO locrefdata;
 
 -- Ensure backup table matches current column set
-ALTER TABLE backup_court_venue ADD COLUMN IF NOT EXISTS service_code VARCHAR(16);
+ALTER TABLE court_venue_backup_2026 ADD COLUMN IF NOT EXISTS service_code VARCHAR(16);
 
--- Temporarily drop FKs that reference court_venue to allow restore
-ALTER TABLE court_district_family_jurisdiction_assoc DROP CONSTRAINT IF EXISTS dfj_court_location_id_fk;
-ALTER TABLE court_district_civil_jurisdiction_assoc DROP CONSTRAINT IF EXISTS dcj_court_location_id_fk;
 
 TRUNCATE TABLE court_venue;
 
 INSERT INTO court_venue
-SELECT * FROM backup_court_venue;
+SELECT * FROM court_venue_backup_2026;
 
 -- Reset sequence to the current max id
 SELECT setval(
@@ -19,9 +16,3 @@ SELECT setval(
          COALESCE(MAX(court_venue_id), 1),
          COALESCE(MAX(court_venue_id), 0) > 0
        ) FROM court_venue;
-
--- Recreate FKs
-ALTER TABLE court_district_family_jurisdiction_assoc ADD CONSTRAINT dfj_court_location_id_fk
-  FOREIGN KEY (court_location_id) REFERENCES court_venue (court_venue_id);
-ALTER TABLE court_district_civil_jurisdiction_assoc ADD CONSTRAINT dcj_court_location_id_fk
-  FOREIGN KEY (court_location_id) REFERENCES court_venue (court_venue_id);
