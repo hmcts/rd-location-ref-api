8505114b1b48dc13ff629e9cd39a7b072fb1e552
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-09-04 17:07:03 +0100

    revertign disabled test cases to fix build

--- src/functionalTest/java/uk/gov/hmcts/reform/lrdapi/RetrieveCourtVenuesBySearchStringFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/lrdapi/RetrieveCourtVenuesBySearchStringFunctionalTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.lrdapi;
 import io.restassured.response.Response;
 import net.serenitybdd.annotations.WithTag;
 import net.serenitybdd.annotations.WithTags;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
